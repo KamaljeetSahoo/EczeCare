53f198a60c438f83f28f3dcb821a48d5963cccb0
Kamaljeet <kamaljeetsahoo@gmail.com>
2022-01-27 00:39:14 +0530

    Authentication with templates

--- EczeCare/settings.py
+++ EczeCare/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication'
 ]
 
 MIDDLEWARE = [
@@ -112,11 +113,20 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+LOGOUT_REDIRECT_URL = "login"
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+MEDIA_URL = ''
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ eczema_profile/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EczemaProfileConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'eczema_profile'
