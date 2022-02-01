d93ab76738d7e3cd5c22975939cdcd44dfe45056
Kamaljeet <kamaljeetsahoo@gmail.com>
2022-02-02 03:10:13 +0530

    patients added to doctor profile db

--- doctor/models.py
+++ doctor/models.py
@@ -4,3 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class DoctorProfile(models.Model):
     doctor = models.ForeignKey(User, on_delete=models.CASCADE)
+    patients = models.ManyToManyField(User, related_name = 'patients')
+
+    def __str__(self):
+        return str(self.doctor.username)
