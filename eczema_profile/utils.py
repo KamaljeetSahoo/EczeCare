b147865ccc96cbde46ed311e8b7f412e07ab92be
Kamaljeet <kamaljeetsahoo@gmail.com>
2022-02-03 23:17:45 +0530

    minor fixes

--- eczema_profile/utils.py
+++ eczema_profile/utils.py
@@ -2,6 +2,44 @@ import cv2
 from django.core.files.base import ContentFile
 from pyowm.owm import OWM
 import math
+
+import torch, torchvision
+
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+
+      
+def get_instance_segmentation_model(num_classes):
+    # load an instance segmentation model pre-trained on COCO
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+
+    # get the number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replace the pre-trained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    # now get the number of input features for the mask classifier
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    hidden_layer = 256
+    # and replace the mask predictor with a new one
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
+                                                       hidden_layer,
+                                                       num_classes)
+
+    return model
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+# our dataset has two classes only - background and person
+num_classes = 2
+
+# get the model using our helper function
+model = get_instance_segmentation_model(num_classes)
+# move model to the right device
+model.to(device)
+
+
 def poem_calc_score(d):
     score=0
     for i in range(1,8):
